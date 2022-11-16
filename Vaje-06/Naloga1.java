730c9e6568b478ac56ab11207d312f67e2577cde
Smotko <114558526+Smotko@users.noreply.github.com>
2022-11-16 07:57:53 +0100

    Add files via upload

---
+++ Vaje-06/Naloga1.java
@@ -0,0 +1,13 @@
+class Naloga1 { //kubernetes meetup Slovenija
+    public static void main(String[] args) {
+        int a[];
+        String b[];
+        char c[];
+        a = new int[] { 1, 2, 3};
+        b = new String[] { "hello", "world"};
+        c = new char[] { 'a', 'b', 'c', 'd'};
+        System.out.println("celaStevila: " + a.length);
+        System.out.println("besede: " + b.length);
+        System.out.println("crke: " + c.length);
+    }
+}
---
+++ Vaje-06/Naloga2.java
@@ -0,0 +1,10 @@
+class Naloga2 {
+    public static void main(String[] args) {
+        int[] a; a = new int[] { 10, 20, 30, 40, 50};
+        String[] b; b = new String[] { "vceraj", "danes", "jutri"};
+        char[] c; c = new char[] { 'x', 'y', 'z', 'w'};
+        System.out.println("celaStevila: " + a.length);
+        System.out.println("besede: " + b.length);
+        System.out.println("crke: " + c.length);
+    }
+}
